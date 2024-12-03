d2fe72c07108fdcaab0fd064deecc15337f4254c
fabianpd19 <fapalma1@espe.edu.ec>
2024-12-02 22:38:46 -0500

    Arreglar index.php + test de TaskManager.js

--- scripts/TaskManager.js
+++ scripts/TaskManager.js
@@ -1,15 +1,14 @@
-
 export default class TaskManager {
-    constructor() {
-        this.tasks = [];
-    }
+  constructor() {
+    this.tasks = [];
+  }
 
-    addTask(task) {
-        this.tasks.push(task);
-        this.tasks.sort((a, b) => new Date(a.start_time) - new Date(b.start_time));
-    }
+  addTask(task) {
+    this.tasks.push(task);
+    this.tasks.sort((a, b) => new Date(a.start_time) - new Date(b.start_time));
+  }
 
-    getTasks() {
-        return this.tasks;
-    }
+  getTasks() {
+    return this.tasks;
+  }
 }
